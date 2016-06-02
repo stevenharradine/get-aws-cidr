23f7b5248d2d3ef16560cbc727cb17525c7b5fc1
Steven Harradine <stevenharradine@gmail.com>
2016-06-02 14:07:00 -0400

    fix white space

--- get-aws-cidr.js
+++ get-aws-cidr.js
@@ -2,21 +2,21 @@ var http = require('https')
 var region = process.argv[2]
 
 http.get("https://ip-ranges.amazonaws.com/ip-ranges.json", function(res){
-    var body = ''
+  var body = ''
 
-    res.on('data', function(chunk){
-        body += chunk
-    })
+  res.on('data', function(chunk){
+    body += chunk
+  })
 
-    res.on('end', function(){
-        var response = JSON.parse(body).prefixes
+  res.on('end', function(){
+    var response = JSON.parse(body).prefixes
         
-        response.forEach (function (item) {
-        	if (item.region === region) {
-        		console.log (item.ip_prefix)
-        	}
-        })
+    response.forEach (function (item) {
+   	  if (item.region === region) {
+        console.log (item.ip_prefix)
+      }
     })
+  })
 }).on('error', function(e){
-      console.log("Got an error: ", e)
+  console.log("Got an error: ", e)
 })
