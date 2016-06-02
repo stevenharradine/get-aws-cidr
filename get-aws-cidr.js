d9e6359d1018e9bd48defc82863ff2de9dd8d1aa
Steven Harradine <stevenharradine@gmail.com>
2016-06-02 14:06:02 -0400

    remove ;

--- get-aws-cidr.js
+++ get-aws-cidr.js
@@ -1,22 +1,22 @@
-var http = require('https');
-var region = process.argv[2];
+var http = require('https')
+var region = process.argv[2]
 
 http.get("https://ip-ranges.amazonaws.com/ip-ranges.json", function(res){
-    var body = '';
+    var body = ''
 
     res.on('data', function(chunk){
-        body += chunk;
-    });
+        body += chunk
+    })
 
     res.on('end', function(){
-        var response = JSON.parse(body).prefixes;
+        var response = JSON.parse(body).prefixes
         
         response.forEach (function (item) {
         	if (item.region === region) {
         		console.log (item.ip_prefix)
         	}
         })
-    });
+    })
 }).on('error', function(e){
-      console.log("Got an error: ", e);
-});
+      console.log("Got an error: ", e)
+})
