2d29e1c1c016723418b3d38270dddee2f5f46db4
Steven Harradine <stevenharradine@gmail.com>
2016-06-02 13:54:26 -0400

    inital commit

---
+++ get-aws-cidr.js
@@ -0,0 +1,22 @@
+var http = require('https');
+var region = process.argv[2];
+
+http.get("https://ip-ranges.amazonaws.com/ip-ranges.json", function(res){
+    var body = '';
+
+    res.on('data', function(chunk){
+        body += chunk;
+    });
+
+    res.on('end', function(){
+        var response = JSON.parse(body).prefixes;
+        console.log (response.length)
+        response.forEach (function (item) {
+        	if (item.region === region) {
+        		console.log (item.ip_prefix)
+        	}
+        })
+    });
+}).on('error', function(e){
+      console.log("Got an error: ", e);
+});
